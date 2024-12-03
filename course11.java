502ef5694d9d0daac5f5860c3ef1e8f3bc7b3814
Andrew <andrewm020451@gmail.com>
2024-12-03 19:14:47 +0300

    first commit2

---
+++ course11.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class course11 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String name = scan.next();
+        System.out.println("Hello, " + name);
+    }
+}
